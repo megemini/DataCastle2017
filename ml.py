15fdde19ad56f9da4f1e4678f16da643d92fd7d8
Shun <megemini@outlook.com>
2017-03-23 14:08:32 +0800

    Add tornado files:ml.py static templates

---
+++ ml.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os.path
+import logging
+
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+        ]
+        settings = dict(
+            ml_title=u"Machine Learning",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            debug=True,
+            default_handler_class = HomeHandler,
+            compress_response = True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+
+
+class HomeHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+
+    logging.debug('Server started : http://localhost' + str(options.port))
+
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
